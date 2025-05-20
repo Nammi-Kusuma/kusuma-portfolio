fe7d83b5e3f0a1e3b719d783fc032c991c6382ff
Nammi-Kusuma <nkusuma892004@gmail.com>
2025-05-20 17:44:44 +0530

    views

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,25 @@ const App = () => {
   const handleLoadingComplete = () => {
     setIsLoading(false);
   };
+
+  useEffect(() => {
+    const gaId = import.meta.env.GOOGLE_ANALYTICS_ID;
+  
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    document.head.appendChild(script1);
+  
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${gaId}');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+  
   
   return (
     <QueryClientProvider client={queryClient}>
