74733dad6d45eae5a64c70e4bdcc7f3eabc4438f
Nammi-Kusuma <nkusuma892004@gmail.com>
2025-05-20 18:04:46 +0530

    views2

--- src/App.tsx
+++ src/App.tsx
@@ -33,25 +33,6 @@ const App = () => {
     setIsLoading(false);
   };
 
-  useEffect(() => {
-    const gaId = process.env.GOOGLE_ANALYTICS_ID;
-  
-    const script1 = document.createElement("script");
-    script1.async = true;
-    script1.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
-    document.head.appendChild(script1);
-  
-    const script2 = document.createElement("script");
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${gaId}');
-    `;
-    document.head.appendChild(script2);
-  }, []);
-  
-  
   return (
     <QueryClientProvider client={queryClient}>
       <LoadingContext.Provider value={{ isLoading }}>
